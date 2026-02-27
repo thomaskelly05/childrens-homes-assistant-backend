5685b01914cc77fbb2f499589962169858a09235
thomaskelly05 <thomas.kelly05@gmail.com>
2026-02-27 22:16:03 +0000

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,12 +1,14 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import os
 
 def get_db():
     conn = psycopg2.connect(
-        host="...",
-        dbname="...",
-        user="...",
-        password="...",
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", 5432),
         cursor_factory=RealDictCursor
     )
     try:
