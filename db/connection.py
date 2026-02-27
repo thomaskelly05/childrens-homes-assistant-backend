9a4324dbbf1d5fae03694ec7ce4744e131fa94b0
thomaskelly05 <thomas.kelly05@gmail.com>
2026-02-27 18:01:44 +0000

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,1 +1,15 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
+def get_db():
+    conn = psycopg2.connect(
+        host="...",
+        dbname="...",
+        user="...",
+        password="...",
+        cursor_factory=RealDictCursor
+    )
+    try:
+        yield conn
+    finally:
+        conn.close()
