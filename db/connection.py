c204c693135f90caa0ae7822a4b74fbaf3e39c7a
thomaskelly05 <thomas.kelly05@gmail.com>
2026-02-28 17:49:46 +0000

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,16 +1,25 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import os
+from urllib.parse import urlparse
 
 def get_db():
+    url = os.getenv("DATABASE_URL")
+    if not url:
+        raise Exception("DATABASE_URL is not set")
+
+    parsed = urlparse(url)
+
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT", 5432),
-        cursor_factory=RealDictCursor
+        host=parsed.hostname,
+        database=parsed.path.lstrip("/"),
+        user=parsed.username,
+        password=parsed.password,
+        port=parsed.port,
+        cursor_factory=RealDictCursor,
+        sslmode="require"
     )
+
     try:
         yield conn
     finally:
