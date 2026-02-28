d0a7af59e5b69c35bbe1c1fcd05aa4df49de17fb
thomaskelly05 <thomas.kelly05@gmail.com>
2026-02-28 22:37:19 +0000

    Update account_routes.py

--- routers/account_routes.py
+++ routers/account_routes.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, Response
-from db.connection import get_db
 from auth.dependencies import get_current_user
 
 router = APIRouter(prefix="/account", tags=["Account"])
