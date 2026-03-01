90fd76f630b06228ba9b8297b65b16956c5b1724
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-01 22:54:10 +0000

    Delete frontend/components directory

--- frontend/components/assistantpanel/assistantpanel.tsx
+++
@@ -1,8 +0,0 @@
-export default function AssistantPanel() {
-  return (
-    <aside className="assistant-panel">
-      <h3>IndiCare Assistant</h3>
-      <p>This space will support your thinking throughout the day.</p>
-    </aside>
-  );
-}
