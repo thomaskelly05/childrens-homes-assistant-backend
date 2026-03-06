94113da36f1bc2a4d11ae738661c7a1090b7f2dc
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-06 13:26:02 +0000

    Update tokens.py

--- auth/tokens.py
+++ auth/tokens.py
@@ -1,13 +1,18 @@
 import jwt
+import os
 from datetime import datetime, timedelta, timezone
 
-JWT_SECRET = "your_jwt_secret_here"
+JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ALGORITHM = "HS256"
 
-def create_session_token(user_id: int, role: str):
+def create_session_token(user_id: int, email: str, role: str, home_id: int):
+
     payload = {
         "sub": user_id,
+        "email": email,
         "role": role,
+        "home_id": home_id,
         "exp": datetime.now(timezone.utc) + timedelta(days=7)
     }
+
     return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
