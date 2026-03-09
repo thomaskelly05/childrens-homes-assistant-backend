3549206b16f3919ca083c64108c1717d9fb0f10a
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-09 22:47:58 +0000

    Update chat.js

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -1,1 +1,46 @@
+let conversation=null
 
+async function sendChat(){
+
+const input=document.getElementById("chatInput")
+
+const text=input.value
+
+input.value=""
+
+const res=await fetch(API+"/chat/",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+credentials:"include",
+
+body:JSON.stringify({
+message:text,
+conversation_id:conversation
+})
+
+})
+
+const reader=res.body.getReader()
+
+const decoder=new TextDecoder()
+
+let ai=""
+
+while(true){
+
+const {done,value}=await reader.read()
+
+if(done)break
+
+ai+=decoder.decode(value)
+
+document.getElementById("aiOutput").innerHTML=ai
+
+}
+
+}
