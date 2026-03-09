b71c50ae9dace280f39a02e8e5bb124d743aaf46
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-09 22:46:50 +0000

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,1 +1,13 @@
+const API="https://api.indicare.co.uk"
 
+async function api(path,options={}){
+
+options.credentials="include"
+
+const res=await fetch(API+path,options)
+
+if(!res.ok)throw new Error("API error")
+
+return res.json()
+
+}
