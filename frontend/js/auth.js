b4c260319abfc1ef3dd3b36ffccecc831e138aaf
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-09 22:47:12 +0000

    Update auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,1 +1,30 @@
+async function login(){
 
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
+
+const res=await fetch(API+"/auth/login",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+credentials:"include",
+
+body:JSON.stringify({email,password})
+
+})
+
+if(res.ok){
+
+window.location="/"
+
+}else{
+
+alert("Login failed")
+
+}
+
+}
