5ae27044d91f25fc588070fb6332f92f91f6bb86
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-10 20:45:34 +0000

    Update conversations.js

--- frontend/js/conversations.js
+++ frontend/js/conversations.js
@@ -1,1 +1,52 @@
+const list = document.getElementById("conversation-list");
 
+let conversations = JSON.parse(
+  localStorage.getItem("conversations") || "[]"
+);
+
+
+export function addConversation(title, id) {
+
+  conversations.push({
+    title: title,
+    id: id
+  });
+
+  localStorage.setItem(
+    "conversations",
+    JSON.stringify(conversations)
+  );
+
+  renderConversations();
+
+}
+
+
+export function renderConversations() {
+
+  list.innerHTML = "";
+
+  conversations.forEach(conv => {
+
+    const div = document.createElement("div");
+
+    div.className = "conversation-item";
+
+    div.innerText = conv.title;
+
+    div.onclick = () => {
+
+      localStorage.setItem("session_id", conv.id);
+
+      location.reload();
+
+    };
+
+    list.appendChild(div);
+
+  });
+
+}
+
+
+renderConversations();
