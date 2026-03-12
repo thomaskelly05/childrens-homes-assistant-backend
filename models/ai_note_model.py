810325698ae08570e51dc1c7fb4dff4638d33503
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-12 15:41:55 +0000

    Update ai_note_model.py

--- models/ai_note_model.py
+++ models/ai_note_model.py
@@ -1,1 +1,24 @@
+from sqlalchemy import Column, Integer, Text, DateTime
+from sqlalchemy.sql import func
+from db.connection import Base
 
+
+class AINote(Base):
+
+    __tablename__ = "ai_notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    child_id = Column(Integer)
+
+    staff_id = Column(Integer)
+
+    transcript = Column(Text)
+
+    ai_draft = Column(Text)
+
+    final_note = Column(Text)
+
+    safeguarding_flag = Column(Integer, default=0)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
