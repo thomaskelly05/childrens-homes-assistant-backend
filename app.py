fa0b126e0770e64b59aa2a53dad5718712f133dc
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-14 23:53:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ from routers.young_people_education_routes import router as young_people_educati
 from routers.young_people_family_routes import router as young_people_family_router
 from routers.young_people_keywork_routes import router as young_people_keywork_router
 from routers.young_people_chronology_routes import router as young_people_chronology_router
+from routers.young_people_compliance_routes import router as young_people_compliance_router
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
@@ -81,6 +82,7 @@ app.include_router(young_people_education_router)
 app.include_router(young_people_family_router)
 app.include_router(young_people_keywork_router)
 app.include_router(young_people_chronology_router)
+app.include_router(young_people_compliance_router)
 
 app.mount("/css", StaticFiles(directory=CSS_DIR), name="css")
 app.mount("/js", StaticFiles(directory=JS_DIR), name="js")
