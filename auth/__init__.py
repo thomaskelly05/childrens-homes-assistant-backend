04ab1af04fdcc2168070194f97fbf1312ddf18b3
thomaskelly05 <thomas.kelly05@gmail.com>
2026-03-30 19:14:15 +0100

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,1 +1,28 @@
+from auth.current_user import get_bearer_token, get_current_user
+from auth.permissions import (
+    home_access_required_from_path,
+    home_access_required_from_query,
+    require_active_subscription,
+    require_admin,
+    require_authenticated_user,
+    require_manager_or_admin,
+    require_provider_admin,
+    require_same_home_or_admin,
+    require_staff_or_manager,
+    role_required,
+)
 
+__all__ = [
+    "get_bearer_token",
+    "get_current_user",
+    "home_access_required_from_path",
+    "home_access_required_from_query",
+    "require_active_subscription",
+    "require_admin",
+    "require_authenticated_user",
+    "require_manager_or_admin",
+    "require_provider_admin",
+    "require_same_home_or_admin",
+    "require_staff_or_manager",
+    "role_required",
+]
