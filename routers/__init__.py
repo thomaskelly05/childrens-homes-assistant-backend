16d6c011077c2ef62d773b6f2970f330873f83c3
thomaskelly05 <thomas.kelly05@gmail.com>
2026-04-15 17:54:29 +0100

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,1 +1,9 @@
+from fastapi import APIRouter
 
+from .assistant_routes import router as operational_assistant_router
+from .young_people_assistant_routes import router as young_people_assistant_router
+
+api_router = APIRouter()
+
+api_router.include_router(operational_assistant_router)
+api_router.include_router(young_people_assistant_router)
