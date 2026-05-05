c3e808d2162b858801ff7d79109e9f55f5a8b4cc
thomaskelly05 <thomas.kelly05@gmail.com>
2026-05-05 19:57:09 +0100

    Register provider intelligence routes

--- core/router_loader.py
+++ core/router_loader.py
@@ -27,6 +27,7 @@ routers.assistant_os_knowledge_routes
 routers.assistant_intelligence_routes
 routers.manager_intelligence_routes
 routers.proactive_intelligence_routes
+routers.provider_intelligence_routes
 routers.young_people_assistant_routes
 routers.operational_intelligence_routes
 routers.inspection_os_routes
