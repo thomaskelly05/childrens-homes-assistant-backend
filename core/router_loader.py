151f5487dd1d98284ccab219d925d1f3611efb09
thomaskelly05 <thomas.kelly05@gmail.com>
2026-05-05 01:24:28 +0100

    Register notifications routes

--- core/router_loader.py
+++ core/router_loader.py
@@ -9,6 +9,7 @@ routers.passkey_routes
 routers.legal_acceptance_routes
 routers.debug_health_routes
 routers.security_routes
+routers.notifications_routes
 routers.frontend_compat
 routers.young_people_shell_item_compat_routes
 routers.young_people_safe_routes
