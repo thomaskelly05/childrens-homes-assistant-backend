c06c8154917f14a563763087c90b17298c373da6
thomaskelly05 <thomas.kelly05@gmail.com>
2026-05-18 12:30:15 +0100

    Apply live OS safeguarding projection patch at startup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import startup_live_child_scope_patch  # noqa: F401
+import startup_live_os_projection_patch  # noqa: F401
 import startup_live_chronology_fallback_patch  # noqa: F401
 from core.app_factory import create_app
 
