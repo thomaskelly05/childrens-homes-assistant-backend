bda82e7dc9111b609f5b61e06254992e64747663
thomaskelly05 <thomas.kelly05@gmail.com>
2026-05-21 12:07:19 +0100

    Enable LifeEcho router patch

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import startup_live_child_scope_patch  # noqa: F401
 import startup_live_chronology_fallback_patch  # noqa: F401
+import startup_life_echo_router_patch  # noqa: F401
 from core.app_factory import create_app
 
 app = create_app()
