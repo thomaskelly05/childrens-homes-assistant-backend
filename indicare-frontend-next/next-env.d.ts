151f94a8f3af2a05aa9f77e6453116f0786527c8
thomaskelly05 <thomas.kelly05@gmail.com>
2026-05-27 22:30:00 +0100

    Add Next env declarations for new IndiCare frontend

---
+++ indicare-frontend-next/next-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// This file is generated in normal Next.js projects and kept here so typecheck has the expected declarations.
